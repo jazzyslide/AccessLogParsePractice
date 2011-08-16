9ca3228ac1da59731485f23ddca9cc653323a754
Hideya Kato <jazzyslide@gmail.com>
2011-08-16 09:09:21 +0900

    ログを実装

--- src/main/java/com/lifexweb/app/hadoop/mapreduce/AccessLogParseMapper.java
+++ src/main/java/com/lifexweb/app/hadoop/mapreduce/AccessLogParseMapper.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -13,22 +15,31 @@ import org.apache.hadoop.mapreduce.Mapper;
 import com.lifexweb.app.hadoop.writable.LogKeyWritable;
 
 public class AccessLogParseMapper extends Mapper<LongWritable, Text, LogKeyWritable, NullWritable> {
+	
 	private LogKeyWritable logKey = new LogKeyWritable();
 	private Date logDatetime;
 	private SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private int urlId;
 	private int cvId;
 	
+	private static Log log = LogFactory.getLog(AccessLogParseMapper.class);
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		super.setup(context);
+		log.info("[MAP] map task started... JobId: " + context.getJobID().getId());
+	}
+
 	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		//TODO logging
 		
 		String line = value.toString();
 		String[] logElements = line.split("\\t");
 		
 		//正しいデータ数かチェックする
 		if (logElements.length > 4) {
-			//TODO logging
+			log.warn("[MAP] SKIP : Invalid Num of Elements:["+ context.getCurrentValue() + "]");
 			return;
 		}
 		try {
@@ -38,15 +49,25 @@ public class AccessLogParseMapper extends Mapper<LongWritable, Text, LogKeyWrita
 			
 			//urlIdとcvIdが「どちらも0以下」や「どちらも正の数」は弾く
 			if ((urlId <= 0 && cvId <= 0) || (urlId > 0 && cvId > 0)) {
-				//TODO logging
+				log.warn("[MAP] SKIP : Invalid match of urlId and cvId:[" + context.getCurrentValue() + "]");
 				return;
 			}
 		} catch (ParseException e) {
-			//TODO logging
+			log.warn("[MAP] SKIP : Parse Error:[" + context.getCurrentValue() + "]");
+			return;
+		} catch (NumberFormatException e) {
+			log.warn("[MAP] SKIP : NumberFormatException:[" + context.getCurrentValue() + "]");
 			return;
 		}
 		
 		logKey.set(logDatetime.getTime(), logElements[1], urlId, cvId);
 		context.write(logKey, NullWritable.get());
+	}
+
+	@Override
+	protected void cleanup(Context context)
+			throws IOException, InterruptedException {
+		log.info("[MAP] map task finished... JobId: " + context.getJobID().getId());
+		super.cleanup(context);
 	}
 }
--- src/main/java/com/lifexweb/app/hadoop/mapreduce/AccessLogParseReducer.java
+++ src/main/java/com/lifexweb/app/hadoop/mapreduce/AccessLogParseReducer.java
@@ -3,6 +3,8 @@ package com.lifexweb.app.hadoop.mapreduce;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,11 +17,20 @@ public class AccessLogParseReducer extends Reducer<LogKeyWritable, NullWritable,
     private SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private LogKeyWritable tmpLogKey = new LogKeyWritable();
 	
+	private static Log log = LogFactory.getLog(AccessLogParseReducer.class);
+    
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		super.setup(context);
+		log.info("[REDUCE] reduce task started... JobId: " + context.getJobID().getId());
+	}
+
 	@Override
 	protected void reduce(LogKeyWritable logKey, Iterable<NullWritable> value, Context context)
 			throws IOException, InterruptedException {
 		
-		for (NullWritable nw : value) {
+		for (@SuppressWarnings("unused") NullWritable nw : value) {
 			if (tmpLogKey.getLogDatetime() == 0 || isUrlLog(tmpLogKey)) {
 				tmpLogKey.set(logKey);
 			} else if (!isUrlLog(tmpLogKey)) {
@@ -38,5 +49,12 @@ public class AccessLogParseReducer extends Reducer<LogKeyWritable, NullWritable,
 
 	private boolean isUrlLog(LogKeyWritable log) {
 		return (log.getUrlId() > 0 && log.getCvId() == 0);
+	}
+	
+	@Override
+	protected void cleanup(Context context)
+			throws IOException, InterruptedException {
+		log.info("[REDUCE] reduce task finished... JobId: " + context.getJobID().getId());
+		super.cleanup(context);
 	}
 }
